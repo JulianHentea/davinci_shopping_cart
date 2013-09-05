aca6d8284a9875540989c7bd4fe57fb347e683f5
Julian Hentea <redpenpress@gmail.com>
2013-09-04 20:14:27 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DavinciShoppingCart::Application.config.secret_key_base = 'ca061cc91307b47f2b58aec00189f08eecebc4e3626bba320ffccd69d3bd00ad1610563a6b8c913bf63f53a475b211767d68e9c00ce590b11e71cd83bd021f9f'
